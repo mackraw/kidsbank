7aa3bc3a4e70738b3035941ba240a1356fd2b399
Maciej Krawczyk <maciekrawczyk@gmail.com>
2021-12-19 14:57:24 -0600

    get account info and transactions

--- application/models/Accounts_model.php
+++ application/models/Accounts_model.php
@@ -20,4 +20,18 @@ class Accounts_model extends CI_Model {
     $query = $this->db->get('accounts');
     return $query->result_array();
   }
+
+  public function get_account($account_id) {
+    $user_id = $this->session->userdata('user_id');
+    $this->db->where('user_id', $user_id);
+    $this->db->where('id', $account_id);
+    $query_account = $this->db->get('accounts');
+    $result = $query_account->row_array();
+
+    $this->db->where('account_id', $account_id);
+    $query_transactions = $this->db->get('transactions');
+    $result['transactions'] = $query_transactions->result_array();
+
+    return $result;
+  }
 }
