145b8094ac699ceef722e5dbfd4006a787669f51
Maciej Krawczyk <maciekrawczyk@gmail.com>
2021-12-18 23:40:07 -0600

    updated template to display data from the database

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -6,12 +6,12 @@ Account page.
   <div class="container-fluid">
     <div class="row">
       <div class="col mb-3 mb-lg-5">
-        <h2 class="display-4">Account Name</h2>
+        <h2 class="display-4">{name}</h2>
       </div>
     </div>
     <div class="row justify-content-between align-items-center mx-3 px-3">
       <span>AVAILABLE BALANCE</span>
-      <span id="totalBalance" class="money-l1 font-weight-bold">$2,455.67</span>
+      <span id="totalBalance" class="money-l1 font-weight-bold">{balance}</span>
     </div>
   </div>
 
@@ -20,7 +20,7 @@ Account page.
 
   <div class="card">
     <div class="card-header">
-      <span class="d-block text-center">My Accounts</span>
+      <span class="d-block text-center">Transaction History</span>
     </div>
     <div class="card-body">
       <div class="row justify-content-between align-items-center mx-3 pb-1 pl-3 pr-3 border-bottom">
@@ -31,18 +31,20 @@ Account page.
         <span class="col-auto col-md-2 text-secondary text-center">Balance</span>
       </div>
 
+      {transactions}
       <div class="row justify-content-between align-items-center mx-3 p-3 border-bottom">
         <div class="col-auto col-md-2">
           <div class="flex-column">
-            <span class="font-weight-bold">APR</span>
-            <span class="font-weight-bold">28</span>
+            <span class="font-weight-bold">{month}</span>
+            <span class="font-weight-bold">{day}</span>
           </div>
         </div>
-        <span class="col-auto col-md-4 text-secondary">Transaction name</span>
-        <span class="col-auto col-md-2 text-secondary text-center">Credit</span>
-        <span class="money-l2 col-auto col-md-2 font-weight-bold text-center">$2,455.78</span>
+        <span class="col-auto col-md-4 text-secondary">{name}</span>
+        <span class="col-auto col-md-2 text-secondary text-center">{type}</span>
+        <span class="money-l2 col-auto col-md-2 font-weight-bold text-center">{amount}</span>
         <span class="money-l2 col-auto col-md-2 font-weight-bold text-center">$2,455.78</span>
       </div>
+      {/transactions}
     </div>
   </div>
 
