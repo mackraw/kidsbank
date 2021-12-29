d44b879a89030b0b409f7180058447e6c5cabe85
Maciej Krawczyk <maciekrawczyk@gmail.com>
2021-12-28 22:55:23 -0600

    scheduled transaction section

--- application/views/pages/account.php
+++ application/views/pages/account.php
@@ -44,6 +44,37 @@ Account page.
     <a href="/account/{account_id}/newtransaction" id="newTransactionBtn" class="btn btn-success mb-5">+ New Transaction</a>
     <a href="/newtransfer" id="newTransferBtn" class="btn btn-success mb-5">+ New Transfer</a>
 
+
+    <div class="card">
+      <div class="card-header">
+        <span class="d-block text-center">Scheduled Transactions</span>
+      </div>
+      <div class="card-body">
+        <div class="row justify-content-between align-items-center mx-3 pb-1 pl-3 pr-3 border-bottom">
+          <span class="col-auto col-md-2 text-uppercase text-secondary">Date</span>
+          <span class="col-auto col-md-4 text-uppercase text-secondary">Transaction name</span>
+          <span class="col-auto col-md-2 text-uppercase text-secondary text-center">Type</span>
+          <span class="col-auto col-md-2 text-uppercase text-secondary text-center">Amount</span>
+        </div>
+
+        {scheduled}
+        <div class="row justify-content-between align-items-center mx-3 p-3 border-bottom">
+          <div class="col-auto col-md-2">
+            <div class="flex-column">
+              <span class="font-weight-bold">{month}</span>
+              <span class="font-weight-bold">{day},</span>
+              <span class="font-weight-bold">{year}</span>
+            </div>
+          </div>
+          <span class="col-auto col-md-4 text-secondary">{name}</span>
+          <span class="col-auto col-md-2 text-secondary text-center">{type}</span>
+          <span class="money-l2 col-auto col-md-2 font-weight-bold text-center">{amount}</span>
+        </div>
+        {/scheduled}
+      </div>
+    </div>
+
+
     <div class="card">
       <div class="card-header">
         <span class="d-block text-center">Transaction History</span>
