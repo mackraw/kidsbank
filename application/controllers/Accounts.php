78c8161378cc91bcc74ad20802bc493214861382
Maciej Krawczyk <maciekrawczyk@gmail.com>
2022-01-06 09:16:16 -0600

    initial commit

---
+++ application/controllers/Accounts.php
@@ -0,0 +1,15 @@
+<?php
+//Purpose: Controller for Accounts.
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+  public function __construct() {
+    parent::__construct();
+    $this->load->library('session');
+    $this->load->helper('url');
+    $this->load->model('users_model');
+    $this->load->model('accounts_model');
+  }
+}
