3ec301ef209a10ba67eb2ce55a00ef67174c5ec6
Maciej Krawczyk <maciekrawczyk@gmail.com>
2022-01-07 12:38:26 -0600

    removed scheduled transactions; added method to update interest

--- application/models/Accounts_model.php
+++ application/models/Accounts_model.php
@@ -28,6 +28,7 @@ class Accounts_model extends CI_Model {
     $result = $query_account->row_array();
 
     $this->db->where('account_id', $account_id);
+    $this->db->order_by('date_time', 'ASC');
     $query_transactions = $this->db->get('transactions');
     $result['transactions'] = $query_transactions->result_array();
 
@@ -53,25 +54,17 @@ class Accounts_model extends CI_Model {
     return $this->db->insert('transactions', $data);
   }
 
-  public function schedule_transaction($data) {
-    return $this->db->insert('scheduled', $data);
-  }
-
-  public function check_scheduled($account_id) {
-    $this->db->where('account_id', (int) $account_id);
-    $query = $this->db->get('scheduled');
-    return $query->result_array();
-  }
-
-  public function remove_scheduled_transaction($trans_id) {
-    $this->db->where('id', $trans_id);
-    return $this->db->delete('scheduled');
+  public function update_interest_date($data) {
+    $this->db->set('last_interest', $data['last_interest']);
+    $this->db->where('id', $data['account_id']);
+    return $this->db->update('accounts');
   }
 
   public function close_account($account_id) {
     if ($this->match_user_account($account_id)) {
       $this->db->where('id', $account_id);
-      return $this->db->delete('accounts');
+      $this->db->set('status', '2');
+      return $this->db->update('accounts');
     }
   }
 }
