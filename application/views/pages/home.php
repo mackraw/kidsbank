4fd1a403c29398040fd0f88db0dfc02a5701c973
Maciej Krawczyk <maciekrawczyk@gmail.com>
2022-01-13 12:23:17 -0600

    added what's inside section

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -10,13 +10,14 @@ Static page for Home.
     <a href="../../../../register" class="btn btn-success btn-lg mt-4">Sign Up</a>
   </div>
 </section>
+
 <section class="bg-light p-5">
-  <div class="container-fluid">
-    <div class="row">
+  <div class="container-fluid mt-3">
+    <div class="row mt-3">
       <div class="col-lg-4 col-sm-10 mx-auto mb-5">
         <h3 class="col text-center mb-3 mb-lg-5 display-3">The Problem</h3>
         <img class="icon mb-3 d-block mx-auto" src="./../../../assets/images/question.svg" alt="Picture of a question mark.">
-        <p class="text-muted">​At Kids' Bank, we believe that the best way for kids to learn about money is to see money in action. In the past, this meant pocket money and piggy banks. But these days, when kids become young adults, they enter the world, which has largely gone cashless. Think checking accounts, credit cards, paying with plastic. Psychologically, this is a lot different from spending cash. Throw debt (AKA credit cards) to the mix, and it's easy to lose track of the balance.​</pc>
+        <p class="text-muted">​At Kids' Bank, we believe that the best way for kids to learn about money is to see money in action. In the past, this meant pocket money and piggy banks. But these days, when kids become young adults, they enter the world, which has largely gone cashless. Think checking accounts and paying with plastic. Psychologically, this is a lot different from spending cash. Throw debt to the mix (AKA credit cards), and it's easy to lose track of the balance.​</pc>
       </div>
       <div class="col-lg-4 col-sm-10 mx-auto mb-5">
         <h3 class="col text-center mb-3 mb-lg-5 display-3">The Solution</h3>
@@ -26,11 +27,12 @@ Static page for Home.
     </div>
   </div>
 </section>
-<section class="p-5">
+
+<section class="bg-dark p-5">
   <div class="container-fluid">
-    <div class="row">
+    <div class="row mt-3">
       <div class="col text-center mb-3 mb-lg-5">
-        <h3 class="display-2">How it works</h3>
+        <h3 class="display-2 mt-3 text-light">How it works</h3>
       </div>
     </div>
     <div class="row justify-content-center">
@@ -39,7 +41,7 @@ Static page for Home.
         <div class="number d-block mx-auto bg-success text-light mb-4">1</div>
         <div class="content my-4">
           <h4 class="h4 my-3 text-success text-center mb-4">Step 1: Open a free account</h4>
-          <p class="text-secondary text-center">Just a few clicks to register. You set up your kid's bank account in minutes.</p>
+          <p class="text-light text-center">Just a few clicks to register. You set up your kid's bank account in minutes.</p>
         </div>
       </div>
 
@@ -47,18 +49,46 @@ Static page for Home.
         <div class="number d-block mx-auto bg-success text-light mb-4">2</div>
         <div class="content my-4">
           <h4 class="h4 my-3 text-success text-center mb-4">Step 2: That's it!</h4>
-          <p class="text-secondary text-center">Well, that's <em>almost</em> it. Once you set up your account, you can start adding transactions. After a while, your kid can view their money (or debt) grow. Check out the cool charts, too!</p>
+          <p class="text-light text-center">Well, that's <em>almost</em> it. Once you set up your account, you can start adding transactions. After a while, your kid can view their money (or debt) grow. Check out the cool charts, too!</p>
         </div>
       </div>
     </div>
   </div>
 </section>
 
-<section class="p-5 bg-success">
+<section class="bg-light p-5">
   <div class="container-fluid">
-    <div class="row">
+    <div class="row mt-3">
       <div class="col text-center mb-5">
-        <h3 class="display-2 text-light">What people say</h3>
+        <h3 class="display-2 mt-3">What's inside</h3>
+      </div>
+    </div>
+
+    <div class="row mb-3 justify-content-center">
+      <img class="col-10 col-md-5 order-md-2 img-fluid mb-3 mb-md-0 d-block mx-auto" src="./../../../assets/images/dash-skw.png" alt="Screenshot of the user dashboard.">
+      <p class="col-auto col-md-5 order-md-1 m-md-auto my-3 text-muted">Access to all of your accounts is available from the Dashboard. You can check balance for each account, as well as see total available balance and total amount owed. From the Dashboard you can also create new accounts and transfer money between accounts.</p>
+    </div>
+    <div class="row mb-3 justify-content-center">
+      <img class="col-10 col-md-5 img-fluid mb-3 mb-md-0 d-block mx-auto" src="./../../../assets/images/banks-skw.png" alt="Screenshot of bank locations.">
+      <p class="col-auto col-md-5 m-md-auto my-3 text-muted">​Find banks based on your location or a search query. Uses your browser's Geolocation API and the LocationIQ API.</p>
+    </div>
+    <div class="row mb-3 justify-content-center">
+      <img class="col-10 col-md-5 order-md-2 img-fluid mb-3 mb-md-0 d-block mx-auto" src="./../../../assets/images/accounts.png" alt="Screenshot of bank account transaction history.">
+      <p class="col-auto col-md-5 order-md-1 m-md-auto my-3 text-muted">View transaction history for each of your accounts. Checking, savings, and credit card accounts available.</p>
+    </div>
+    <div class="row mb-3 justify-content-center">
+      <img class="col-10 col-md-5 img-fluid mb-3 mb-md-0 d-block mx-auto" src="./../../../assets/images/transactions-skw.png" alt="Screenshot of available transactions.">
+      <p class="col-auto col-md-5 m-md-auto my-3 text-muted">Record transfers between checking and savings accounts. Credit card accounts can make purchases. Make sure to pay off your credit card balance by the end of the month or you will be charged interest on the first of the following month.</p>
+    </div>
+  </div>
+  </div>
+</section>
+
+<section class="bg-success p-5">
+  <div class="container-fluid">
+    <div class="row mt-3">
+      <div class="col text-center mb-5">
+        <h3 class="display-2 mt-3 text-light">What people say</h3>
       </div>
     </div>
     <div class="row justify-content-center">
